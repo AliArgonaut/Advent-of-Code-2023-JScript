796422cace95c063d61df5c0cc19b5903345f875
Lloyd <Lloydabonds@gmail.com>
2024-02-23 17:50:48 -0500

    started working in problem two, haven't gotten too far.

--- Day01/Day01.js
+++ Day01/Day01.js
@@ -1,11 +1,10 @@
 var fs = require('fs');
+
 const newArr = fs.readFileSync('C:/Users/lloyd/Desktop/Coding/web development/Advent of Code 2023/Day01/input.txt', 'utf-8').trim().split("\n").map(lines => lines.replace("\r", ""));
 
-function wordNums(){
-    
-}
 
-function findFirstLastNums(){
+function PartOneSolution(){
+
     let output = [];
     for (i = 0; i < newArr.length; i++){
         oneLine = newArr[i].split("")
@@ -16,6 +15,3 @@ function findFirstLastNums(){
     FinalResult = output.map(str => parseInt(str, 10)).reduce((p, c) => {return p + c}, 0)
     console.log(FinalResult)
 }
-
-findFirstLastNums()
-
